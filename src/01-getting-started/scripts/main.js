ba58fc26e4bb0f51ce72b0f09ca23ce9d656363a
Adriana Silva <achapellin@gmail.com>
2019-10-12 23:11:04 -0600

    reorganized version

--- src/01-getting-started/scripts/main.js
+++ src/01-getting-started/scripts/main.js
@@ -8,7 +8,6 @@ const plusBtn = document.getElementById("plusBtn");
 const minusBtn = document.getElementById("minustBtn");
 const multBtn = document.getElementById("multBtn");
 const divBtn = document.getElementById("divBtn");
-let num1, num2;
 
 
 idNumber.addEventListener('change', (() => {
@@ -26,47 +25,45 @@ const userFace = {
     },
 
     getNumber: () => {
-        num1 = Number(firstField.value);
-        num2 = Number(secondField.value);
+        let num1 = Number(firstField.value);
+        let num2 = Number(secondField.value);
+        return {n1: num1, n2: num2};
     },
 
-    displayResults : (result, operator) => {
-        if ((isNaN(num1)) || (isNaN(num2))) {
+    displayResults : (numbers, result, operator) => { 
+         resultField.textContent = (numbers.n1+ " " + operator + " " + numbers.n2+ " = " + result);
+    },
+
+    operations : (operator) => {
+        let numbers = userFace.getNumber();
+       
+       
+        if ((isNaN(numbers.n1)) || (isNaN(numbers.n2))) {
             resultField.textContent = "Please enter two numbers!";
             return;
-        };    
-        resultField.textContent = (num1+ " " + operator + " " + num2+ " = " + result);
+        }
+        else if (operator === "+") {
+            let result = calculations.add(numbers.n1, numbers.n2);
+            userFace.displayResults(numbers, result, "+");
+            return result;
+        }
+        else if (operator === "-") {
+            let result = calculations.subtract(numbers.n1, numbers.n2);
+            userFace.displayResults(numbers, result, "-");
+            return result;
+        }
+        else if (operator === "*") {
+            let result = calculations.multiplication(numbers.n1, numbers.n2);
+            userFace.displayResults(numbers, result, "*");
+            return result;
+        }
+        else{
+            let result = calculations.division(numbers.n1, numbers.n2);
+            userFace.displayResults(numbers, result, "/");
+            return result;
+        }
     },
 
-    onPlusClicked: () => {
-        userFace.getNumber();
-        let result = calculations.add(num1, num2);
-        userFace.displayResults(result, "+");
-        return result; 
-    },
-
-    onMinusClicked: () => {
-        userFace.getNumber();
-        let result = calculations.subtract(num1, num2);
-        userFace.displayResults(result, "-");
-        return result;   
-    },
-
-
-    onMultiplicationClicked: () => {
-        userFace.getNumber();
-        let result = calculations.multiplication(num1, num2);
-        userFace.displayResults(result, "*");
-        return result;
-    } ,
-
-    onDivisionClicked: () => {
-        userFace.getNumber();
-        let result = calculations.division(num1, num2);
-        userFace.displayResults(result, "/");
-        return result;
-    },
-    
     onClearClicked: () => {
         firstField.value = "Type 1st Number";
         secondField.value = "Type 2nd Number";
@@ -78,7 +75,7 @@ const userFace = {
 clearBtn.addEventListener("click", userFace.onClearClicked);
 firstField.addEventListener("click", userFace.overFirstField);
 secondField.addEventListener("click", userFace.overSecondField);
-plusBtn.addEventListener("click", userFace.onPlusClicked);
-minusBtn.addEventListener("click", userFace.onMinusClicked);
-multBtn.addEventListener("click", userFace.onMultiplicationClicked);
-divBtn.addEventListener("click", userFace.onDivisionClicked);
+plusBtn.addEventListener("click", function() { userFace.operations("+");});
+minusBtn.addEventListener("click", function() { userFace.operations("-");});
+multBtn.addEventListener("click", function() { userFace.operations("*");});
+divBtn.addEventListener("click", function() { userFace.operations("/");});
