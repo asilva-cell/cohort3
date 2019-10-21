4e624496f92ce2f43afb77074e99bf2e9d614dd7
Adriana Silva <achapellin@gmail.com>
2019-10-20 23:51:53 -0600

    Comp110 HTML and event listeners

---
+++ src/02-dom/scripts/calculator.js
@@ -0,0 +1,24 @@
+
+
+const calculations = {
+    add: (num1, num2) => {
+        return num1 + num2;
+    },
+
+    subtract: (num1, num2) => {
+        return num1 - num2;
+    },
+
+    multiply: (num1, num2) => {
+        return num1 * num2;
+    },
+
+    divide: (num1, num2) => {
+        return num1 / num2;
+    }
+    
+};
+
+
+
+export {calculations};
---
+++ src/02-dom/scripts/main.js
@@ -0,0 +1,18 @@
+
+//import {functions} from "./functions.js";
+
+const oList = document.getElementById("idOList");
+
+home.addEventListener('click', (event) => {
+    console.log(event);
+});
+
+
+idShow.addEventListener('click', (event) => {
+    console.log(oList.children);
+});
+
+// idAdd.addEventListener('click', (event) => {
+//     console.log(event);
+// });
+
