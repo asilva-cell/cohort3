2e43970bb83e84aa66b734f20300500dd2cc4c11
Adriana Silva <achapellin@gmail.com>
2020-02-16 20:44:52 -0700

    tested with python3.7

--- py-training/__python37-venv__/bin/activate.fish
+++ py-training/__python37-venv__/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/adriana/workspace/evolveu/py-training/__python37-venv__"
+set -gx VIRTUAL_ENV "/home/adriana/code/cohort3/py-training/__python37-venv__"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
