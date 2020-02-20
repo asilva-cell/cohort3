325f1728010a9e0f5e13e6289aad48025ffb10ad
Adriana Silva <achapellin@gmail.com>
2020-02-19 23:54:45 -0700

    create report - to be refactored

--- py-training/src/com220-files/read_csv.py
+++ py-training/src/com220-files/read_csv.py
@@ -21,5 +21,21 @@ with open('Census_by_Community_2018.csv', 'r') as file:
         people_count_by_sector[row['SECTOR']]['population'] += int(row['RES_CNT'])
         people_count_by_sector[row['SECTOR']]['count'] += 1
 
-    print(people_count_by_zone_type)
-    print(people_count_by_sector)
+with open('Census_Report.csv', 'w+') as report:
+    string =""
+    string += " CENSUS REPORT ".center(70, "-") + "\n"
+    string += " POPULATION BY ZONING TYPE ".center(70, "-") + "\n"
+    string += "Zone Type".ljust(20) + "Population".ljust(20) + "Lines".ljust(20) + "\n"
+    string += "-".center(70, "-") + "\n"
+    for zone_type, data in people_count_by_zone_type.items():
+       string += f"{zone_type}".ljust(20) + f"{data['population']}".ljust(20) + f"{data['count']}".ljust(20) +"\n"
+
+    string +=  "\n""\n"
+
+    string += " POPULATION BY SECTOR ".center(70, "-") + "\n"
+    string += "Sector".ljust(20) + "Population".ljust(20) + "Lines".ljust(20) + "\n"
+    string += "-".center(70, "-") + "\n"
+    for sector, data in people_count_by_sector.items():
+       string += f"{sector}".title().ljust(20) + f"{data['population']}".ljust(20) + f"{data['count']}".ljust(20) +"\n"
+    
+    report.write(string)
