25f52ca8638199a7e377b5930384b5bd6443a3cc
Adriana Silva <achapellin@gmail.com>
2020-03-02 16:10:55 -0700

    test

--- py-training/src/com220-files/read_directory.py
+++
@@ -1,8 +0,0 @@
-import os
-from os.path import join, getsize
-
-
-def dir_size(path):
-    file_count = len(os.listdir(path))
-    path_size = os.path.getsize(path)
-    return f'There are {file_count} files in the directory, which has a total size of {path_size} bytes.'
--- py-training/src/refresher.py
+++
@@ -1,6 +0,0 @@
-my_list=[1,49,50]
-my_tuple=(50,)
-set1 = {14, 5, 9, 31, 12, 77, 67, 8}
-set2 = {5, 77, 9, 12}
-
-print(set1.intersection(set2))
